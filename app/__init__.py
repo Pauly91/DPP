1fdfe6fff4f53b7207b25ad5d43284dd745b66f3
Pauly91 <abelpaul91@gmail.com>
2021-09-30 17:00:47 +0530

    app is running

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
 
-# Initialize the app
+
+from flask import Flask
+from flask_restful import Api
+from app.views import queryHandler
+
 app = Flask(__name__, instance_relative_config=True)
 
-# Load the config file
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 app.config.from_object('config')
+
+api = Api(app)
+api.add_resource(queryHandler, '/')
--- app/views.py
+++ app/views.py
@@ -1,17 +1,11 @@
-from flask import render_template
+
 from flask_restful import Resource, Api
 
-from app import app
 
-api = Api(app)
-
-
-class TodoSimple(Resource):
+class queryHandler(Resource):
     def get(self):
         return {"hello": "Hello Worldsadas"}
 
-
-api.add_resource(TodoSimple, '/')
 
 '''
 tuts: https://scotch.io/tutorials/getting-started-with-flask-a-python-microframework
